8f86c36af77dc7e8e635173744dc333ea2c40181
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-07-27 19:56:14 +0430

    implement About component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cocktails from './components/cocktails'
+import About from './components/About'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
@@ -13,6 +14,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <Cocktails />
+        <About />
     </main>
   )
 }
