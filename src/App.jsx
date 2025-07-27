6c9e7463249fae4947a076b9525386978bf920a2
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-07-27 21:32:21 +0430

    implement Menu component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero'
 import Cocktails from './components/cocktails'
 import About from './components/About'
 import Art from './components/Art'
+import Menu from './components/Menu'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
@@ -17,6 +18,7 @@ const App = () => {
         <Cocktails />
         <About />
         <Art />
+        <Menu />
     </main>
   )
 }
