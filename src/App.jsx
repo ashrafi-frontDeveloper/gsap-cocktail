f87840b7c8d94f07c9ca69e56a106a51621f7c1b
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-07-27 21:34:55 +0430

    implement Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Cocktails from './components/cocktails'
 import About from './components/About'
 import Art from './components/Art'
 import Menu from './components/Menu'
+import Contact from './components/Contact'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
@@ -19,9 +20,9 @@ const App = () => {
         <About />
         <Art />
         <Menu />
+        <Contact />
     </main>
   )
 }
 
 export default App
-
